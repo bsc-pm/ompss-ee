1edadda38e4d2fb4ce9b9bc35af3f3c5e68f0912
Xavier Teruel <xavier.teruel@bsc.es>
2014-05-27 16:37:06 +0200

    Adding new configure options, and more verbosity

--- source/ompss-ee/configure.sh
+++ source/ompss-ee/configure.sh
@@ -1,43 +1,70 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 DIRNAME=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
-# Other Machines
-#export MKL_LIB_DIR=
-#export MKL_INC_DIR=
-
-# Marenostrum (@BSC)
-#export MKL_LIB_DIR=/opt/intel/mkl/lib/intel64/
-#export MKL_INC_DIR=/opt/intel/mkl/include/
-
-# Minotauro (@BSC)
-export MKL_LIB_DIR=/opt/compilers/intel/mkl/lib/intel64/
-export MKL_INC_DIR=/opt/compilers/intel/mkl/include/
-
-# Configure OmpSs
-export OMPSS_HOME=/apps/PM/ompss/2014-04-10/
-export PATH=$OMPSS_HOME/bin:$PATH
-
-# Configure Extrae
-export EXTRAE_HOME=/apps/CEPBATOOLS/extrae/latest/default/64/
-export PATH=$EXTRAE_HOME/bin/:$PATH
-
-# Configure Paraver
-export PARAVER_HOME=/apps/CEPBATOOLS/wxparaver/latest/
-export PATH=$PARAVER_HOME/bin:$PATH
-
-# Setting libraries
-export LD_LIBRARY_PATH=$MKL_LIB_DIR:$LD_LIBRARY_PATH
-
-# Checking configuration
-if [ ! -f $DIRNAME/common-files/sched-job ];
-then
-   echo WARNING: Job schedule file is not configured for this machine!
+if [ "X$BSC_MACHINE" == "Xmn3" ]; then
+  # (@BSC) Marenostrum III section
+  export MKL_LIB_DIR=/opt/intel/mkl/lib/intel64/
+  export MKL_INC_DIR=/opt/intel/mkl/include/
+elif [ "X$BSC_MACHINE" == "Xnvidia" ]; then
+  # (@BSC) Minotauro section
+  export MKL_LIB_DIR=/opt/compilers/intel/mkl/lib/intel64/
+  export MKL_INC_DIR=/opt/compilers/intel/mkl/include/
+else
+  # Other Machines (AD-HOC) section
+  export MKL_LIB_DIR=
+  export MKL_INC_DIR=
 fi
 
-if [ ! -f $MKL_LIB_DIR/libmkl_sequential.so ];
-then
-   echo WARNING: MKL library is not found, will not be compiled!
+# Configure OmpSs + Extrae + Paraver
+export OMPSS_HOME=/apps/PM/ompss/2014-04-10
+export EXTRAE_HOME=/apps/CEPBATOOLS/extrae/latest/default/64
+export PARAVER_HOME=/apps/CEPBATOOLS/wxparaver/latest
+
+# Setting environment variables 
+export PATH=$OMPSS_HOME/bin:$PATH
+export PATH=$EXTRAE_HOME/bin/:$PATH
+export PATH=$PARAVER_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$MKL_LIB_DIR:$LD_LIBRARY_PATH
+
+echo Basic configuration...
+
+# Checking configuration, verbosing configuration
+if [ ! -f $OMPSS_HOME/bin/mcc ]; then
+   echo \ \ WARNING: Mercurium compiler not found!
+else
+   echo \ \ Mercurium compiler at $OMPSS_HOME/bin 
+fi 
+
+if [ ! -f $EXTRAE_HOME/bin/mpi2prv ]; then
+   echo \ \ WARNING: Extrae library not found!
+else
+   echo \ \ Extrae library at $EXTRAE_HOME/bin 
+fi 
+
+if [ ! -f $PARAVER_HOME/bin/wxparaver ]; then
+   echo \ \ WARNING: Paraver utility not found!
+else
+   echo \ \ Paraver utility at $PARAVER_HOME/bin 
+fi 
+
+if [ ! -f $DIRNAME/common-files/sched-job ]; then
+   echo WARNING: Job schedule file is not configured for this machine!
+else
+   if [ -s $DIRNAME/common-files/sched-job ] ; then
+      echo Not using any job schedule feature for this machine
+   else
+      echo Job schedule configuration file...
+      cat  $DIRNAME/common-files/sched-job
+   fi
+fi
+
+echo Aditional libraries...
+
+if [ ! -f $MKL_LIB_DIR/libmkl_sequential.so ]; then
+   echo \ \ WARNING: MKL library is not found, will not be compiled!
+else
+   echo \ \ MKL library at $MKL_LIB_DIR
 fi
 
 if [ "X$1" == "X--pack" ];
