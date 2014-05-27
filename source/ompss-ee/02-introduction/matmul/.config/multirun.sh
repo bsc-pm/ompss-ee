ccbe25f3a7f6962c63c9cbdf82fd47ecb855af70
Xavier Teruel <xavier.teruel@bsc.es>
2014-05-27 11:53:19 +0200

    Adding matmul exercise

---
+++ source/ompss-ee/02-introduction/matmul/.config/multirun.sh
@@ -0,0 +1,12 @@
+PROGRAM=matmul-p
+
+export IFS=";"
+
+THREADS="01;02;03;04;05;06;07;08;09;10;11;12"
+SIZES="16"
+
+for size in $SIZES; do
+  for thread in $THREADS; do
+    NX_GPUS=0 NX_THREADS=$thread ./$PROGRAM $size
+  done
+done
---
+++ source/ompss-ee/02-introduction/matmul/.config/run-once.sh
@@ -0,0 +1,6 @@
+PROGRAM=matmul-p
+
+export NX_THREADS=1
+
+./$PROGRAM 16
+
