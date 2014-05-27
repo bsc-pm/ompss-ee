23f86b02b26f8e046b6038eaa3a4309dd9706442
Xavier Teruel <xavier.teruel@bsc.es>
2014-05-27 11:57:27 +0200

    Adding multisort benchmark

---
+++ source/ompss-ee/02-introduction/multisort/.config/multirun.sh
@@ -0,0 +1,18 @@
+PROGRAM=multisort-p
+
+export IFS=";"
+
+THREADS="01;02;03;04;05;06;07;08;09;10;11;12"
+VSIZE="65536"
+SEQ_SORT="256"
+SEQ_MERGE="512"
+
+for size in $VSIZE; do
+  for seq_sort in $SEQ_SORT; do
+    for seq_merge in $SEQ_MERGE; do
+      for thread in $THREADS; do
+        NX_GPUS=0 NX_THREADS=$thread ./$PROGRAM $size $seq_sort $seq_merge
+      done
+    done
+  done
+done
---
+++ source/ompss-ee/02-introduction/multisort/.config/run-once.sh
@@ -0,0 +1,6 @@
+PROGRAM=multisort-p
+
+export NX_THREADS=1
+
+./$PROGRAM 65536 256 512
+
