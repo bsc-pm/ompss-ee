62b9558b5ccc62a9460d38498b67ad2faf671f70
Xavier Teruel <xavier.teruel@bsc.es>
2014-07-10 17:50:25 +0200

    Fixing matmul opencl version

--- source/ompss-ee/03-gpu-devices/matmul-opencl/.config/multirun.sh
+++ source/ompss-ee/03-gpu-devices/matmul-opencl/.config/multirun.sh
@@ -2,10 +2,11 @@ PROGRAM=matmul-p
 
 export NX_THREADS=1
 export NX_OPENCL_MAX_DEVICES=2 #max number of opencl devices (GPUs in this case) to use
+export NX_OPENCL_DEVICE_TYPE=gpu
 
 # Creating input file
 touch test.in
-echo "8192 8192 8192 3" > test.in
+echo "4096 4096 4096 3" > test.in
 
 # Executing the program
 ./$PROGRAM
