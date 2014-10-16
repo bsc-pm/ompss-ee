32b7b833ab6d4a1cf416cf638184be1dcdf79f9e
Guillermo Miranda <guillermo.miranda@bsc.es>
2014-10-16 10:25:58 +0200

    heat solver with dependencies

--- source/ompss-ee/04-mpi+ompss/heat/README.rst
+++ source/ompss-ee/04-mpi+ompss/heat/README.rst
@@ -9,3 +9,8 @@ This codes performs a ... hybrid MPI/OmpSs implementation.
   You can dowload this code visiting the url http://pm.bsc.es *OmpSs Examples and Exercises*'s
   (code) link. This version of matrix multiply kernel is included inside the  *04-mpi+ompss*'s directory.
 
+.. note::
+  You need to specify the number of MPI tasks per node. In Marenostrum you can do this
+  by adding <<#BSUB -R "span[ptile=1]">> to your job script.
+
+
--- source/ompss-ee/04-mpi+ompss/heat/solver-mpi-ompss.c
+++ source/ompss-ee/04-mpi+ompss/heat/solver-mpi-ompss.c
@@ -7,33 +7,47 @@
  */
 double relax_jacobi (double *u, double *utmp, unsigned sizex, unsigned sizey)
 {
+    double (*um)[sizey] = u, (*utmpm)[sizey] = utmp;
+
     double diff, sum=0.0;
     int nbx, bx, nby, by;
+    int inf_i, sup_i, inf_j, sup_j;
+    int up, left, right, down;
   
     nbx = NB;
     bx = sizex/nbx;
     nby = NB;
     by = sizey/nby;
-    for (int ii=0; ii<nbx; ii++)
-        for (int jj=0; jj<nby; jj++) {
-            #pragma omp task  shared (sum) label (nested_comp)
+    for (int ii=1; ii<sizex-1; ii+=bx)
+        for (int jj=1; jj<sizey-1; jj+=by) {
+	        inf_i = ii; inf_j=jj; 
+	        sup_i = (ii+bx)<sizex-1 ? ii+bx : sizex-1; 	
+	        sup_j = (jj+bx)<sizey-1 ? jj+by : sizey-1; 
+
+	        up = (ii-bx)<0? 0 : ii-bx;
+	        left = (jj-by)<0 ? 0 : jj - by; 
+	        down = (ii+bx)> sizex-1? sizex-1 : ii+bx;
+            right = (jj+by)>sizey-1 ? sizey - 1 : jj+by; 
+
+            #pragma omp task in (um[up][jj], um[ii][left], um[down][jj], um[ii][right]) out (utmpm[ii][jj])  concurrent(sum) firstprivate(ii, left, down, jj, right, up) label(compute_jacobi)
             {
-                double local_sum = 0.0;
-                for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++) 
-                    for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
-                        utmp[i*sizey+j]= 0.25 * (u[ i*sizey     + (j-1) ]+  // left
-                            u[ i*sizey     + (j+1) ]+  // right
-                            u[ (i-1)*sizey + j     ]+  // top
-                            u[ (i+1)*sizey + j     ]); // bottom
-                        diff = utmp[i*sizey+j] - u[i*sizey + j];
+	            double local_sum=0.0;
+                for (int i=inf_i; i<sup_i; i++) 
+                    for (int j=inf_j; j<sup_j; j++) {
+                        utmpm[i][j]= 0.25 * (um[ i][j-1 ]+  // left
+			                 um[ i][(j+1) ]+  // right
+	                         um[(i-1)][ j]+  // top
+	                         um[ (i+1)][ j ]); // bottom
+                        diff = utmpm[i][j] - um[i][j];
                         local_sum += diff * diff; 
                     }
-                //#pragma omp atomic
-                sum += local_sum;
+
+                #pragma omp atomic
+	            sum += local_sum;
             }
-}
+	    }
+
 #pragma omp taskwait
-    printf ("Partial residual %lf\n", sum);
     return sum;
 }
 
