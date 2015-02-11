09e1b7542b8e3761d093ff6c86c1e7fc2efcd62c
Guillermo Miranda Alamo <guillermo.miranda@bsc.es>
2015-02-11 11:36:14 +0100

    NX_OPENCL_DEVICE_TYPE must be in capital letters

--- source/ompss-ee/03-gpu-devices/matmul-opencl/.config/run-once.sh
+++ source/ompss-ee/03-gpu-devices/matmul-opencl/.config/run-once.sh
@@ -2,7 +2,7 @@ PROGRAM=matmul-p
 
 export NX_THREADS=1
 export NX_OPENCL_MAX_DEVICES=2 #max number of opencl devices (GPUs in this case) to use
-export NX_OPENCL_DEVICE_TYPE=gpu
+export NX_OPENCL_DEVICE_TYPE=GPU
 
 # Creating input file
 touch test.in
