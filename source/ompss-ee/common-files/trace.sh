a10dccf7c48c7d0aa52b15eeab248beef74f7c11
Guillermo Miranda Alamo <guillermo.miranda@bsc.es>
2015-06-30 11:55:54 +0200

    Common trace.sh has instructions on how to instrument MPI+OpenMP

--- source/ompss-ee/common-files/trace.sh
+++ source/ompss-ee/common-files/trace.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Uncomment following line to trace MPI programs
+# Uncomment the following line to trace MPI+OmpSs programs
 #export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanosmpitrace.so
+
+# Uncomment the following line to trace MPI+OpenMP (GNU) programs
+#export LD_PRELOAD=${EXTRAE_HOME}/lib/libompitrace.so
 
 export EXTRAE_CONFIG_FILE=extrae.xml
 export NX_INSTRUMENTATION=extrae
