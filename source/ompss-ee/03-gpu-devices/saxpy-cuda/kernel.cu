7e624d10dc8de922b1df4c65d3d298f86fc96610
Xavier Teruel <xavier.teruel@bsc.es>
2015-07-02 17:10:01 +0200

    Cleaning up saxpy-cuda

--- source/ompss-ee/03-gpu-devices/saxpy-cuda/kernel.cu
+++ source/ompss-ee/03-gpu-devices/saxpy-cuda/kernel.cu
@@ -2,6 +2,6 @@
 
 __global__ void saxpy(int n, float a, float* x, float* y)
 {
-    int i = blockIdx.x*blockDim.x+threadIdx.x;
-    if(i < n) y[i] = a * x[i] + y[i];
+   int i = blockIdx.x * blockDim.x + threadIdx.x;
+   if(i < n) y[i] = a * x[i] + y[i];
 }
--- source/ompss-ee/03-gpu-devices/saxpy-cuda/saxpy.c
+++ source/ompss-ee/03-gpu-devices/saxpy-cuda/saxpy.c
@@ -1,26 +1,29 @@
 #include <kernel.h>
 
 #define N 1024
-int main(int argc, char* argv[]) {
-    float  a, x[N], y[N];
-	a=5;
-	int i;
-	for (i=0; i<N; ++i){
-		x[i]=i;
-		y[i]=i+2;
-	}
 
-    saxpy(N, a, x,  y);
+int main(int argc, char* argv[])
+{
+   float a=5, x[N], y[N];
+   int i;
+
+   for (i=0; i<N; ++i){
+      x[i]=i;
+      y[i]=i+2;
+   }
+
+   saxpy(N, a, x,  y);
 
 #pragma omp taskwait
-	
-	//Check results	
-	for (i=0; i<N; ++i){
-		if (y[i]!=a*i+(i+2)){
-			printf("Error when checking results, in position %d\n",i);
-			return -1;
-		}
-	}
-	printf("Results are correct\n");
-    return 0;
+
+   //Check results	
+   for (i=0; i<N; ++i){
+      if (y[i]!=a*i+(i+2)){
+         printf("Error when checking results, in position %d\n",i);
+         return -1;
+      }
+   }
+
+   printf("Results are correct\n");
+   return 0;
 }
