60f365b04153e6fa06231cfd428b752f2e24289f
Guillermo Miranda Alamo <guillermo.miranda@bsc.es>
2015-12-02 14:55:20 +0100

    Heat OpenMP: minor fixes in the scripts

--- source/ompss-ee/05-openmp/heat/.config/run-once.sh
+++ source/ompss-ee/05-openmp/heat/.config/run-once.sh
@@ -2,10 +2,10 @@
 # Uncomment to instrument
 PROGRAM=heat-i
 
-# Run with 2 threads per MPI process in the same node
-
+# Run with 2 threads
 export OMP_NUM_THREADS=2
 
 # Uncomment to instrument
+INST=./trace.sh
 
 $INST ./$PROGRAM test512-jacobi-small.dat test512-jacobi-16.ppm
