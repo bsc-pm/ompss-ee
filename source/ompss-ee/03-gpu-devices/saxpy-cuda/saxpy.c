26c7c3aadd45a2377e98d4c9db4a44bc4ce34ccf
Xavier Teruel <xavier.teruel@bsc.es>
2016-07-13 17:30:46 +0200

    Computing saxpy kernel by blocks

--- source/ompss-ee/03-gpu-devices/saxpy-cuda/saxpy.c
+++ source/ompss-ee/03-gpu-devices/saxpy-cuda/saxpy.c
@@ -1,18 +1,30 @@
 #include <kernel.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define N 1024
+#define N 1024*1024
+#define BS  64*1024
 
 int main(int argc, char* argv[])
 {
-   float a=5, x[N], y[N];
+   float a=5, *x, *y;
    int i;
+
+   x = (float *) malloc(N*sizeof(float));
+   y = (float *) malloc(N*sizeof(float));
 
    for (i=0; i<N; ++i){
       x[i]=i;
       y[i]=i+2;
    }
 
-   saxpy(N, a, x,  y);
+   if ( N % BS != 0 ) {
+      printf("Size %d should divide block size %d\n", N, BS);
+      return -1;
+   }
+
+   for (i=0; i<N; i+=BS )
+      saxpy(BS, a, &x[i],  &y[i]);
 
 #pragma omp taskwait
 
