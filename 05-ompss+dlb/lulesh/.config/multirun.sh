d1b85a7899935436c0b6e20c41d0df8707850cde
Victor Lopez <victor.lopez@bsc.es>
2017-10-04 17:56:20 +0200

    Add DLB example: Lulesh

---
+++ 05-ompss+dlb/lulesh/.config/multirun.sh
@@ -0,0 +1,15 @@
+PROGRAM=lulesh2.0-p
+
+# Uncomment to enable DLB
+# export NX_ARGS+=" --thread-manager=dlb"
+# export LB_POLICY="auto_LeWI_mask"
+# export LB_LEND_MODE="BLOCK"
+# export OMPSSEE_LD_PRELOAD=$DLB_HOME/lib/libdlb_mpi.so
+# export I_MPI_WAIT_MODE=1
+
+export NX_ARGS+=" --force-tie-master --warmup-threads"
+
+for i in $(seq 1 3) ; do
+    mpirun -n 27 env LD_PRELOAD=$OMPSSEE_LD_PRELOAD ./$PROGRAM -i 15 -b 8 -s 100 \
+        | tac | grep -m 1 'Elapsed time'
+done
--- 05-ompss+dlb/lulesh/.config/run-once.sh
+++ 05-ompss+dlb/lulesh/.config/run-once.sh
@@ -1,4 +1,4 @@
-PROGRAM=mpi_ompss_pils-i
+PROGRAM=lulesh2.0-i
 
 # Uncomment to instrument
 # export INST=./trace.sh
@@ -6,9 +6,9 @@ PROGRAM=mpi_ompss_pils-i
 # Uncomment to enable DLB
 # export NX_ARGS+=" --thread-manager=dlb"
 # export LB_POLICY="auto_LeWI_mask"
-
-# Uncomment to enable DLB MPI interception
+# export LB_LEND_MODE="BLOCK"
 # export OMPSSEE_LD_PRELOAD=$DLB_HOME/lib/libdlb_mpi_instr.so
+# export I_MPI_WAIT_MODE=1
 
-export NX_ARGS+=" --force-tie-master"
-mpirun $INST ./$PROGRAM /dev/null 1 10 500
+export NX_ARGS+=" --force-tie-master --warmup-threads"
+mpirun -n 27 $INST ./$PROGRAM -i 15 -b 8 -s 100
